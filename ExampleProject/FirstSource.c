25d972e0e6e6c702832dc8b4a84fd383d7569e3d
rranno <rranno@hotmail.it>
2018-03-28 16:59:48 +0200

    modification

--- ExampleProject/FirstSource.c
+++ ExampleProject/FirstSource.c
@@ -4,9 +4,6 @@
  *  Created on: 28/mar/2018
 <<<<<<< HEAD
  *      Author: Raffaele Ranno
-=======
- *      Author: Giuseppe Ranno
->>>>>>> branch 'master' of https://github.com/rranno/hello-world.git
- */
+
 
 
