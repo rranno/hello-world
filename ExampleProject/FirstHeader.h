f4746c3babc7f9fc9e1c5601f8fec225ae5cd372
rranno <rranno@hotmail.it>
2018-03-28 19:40:08 +0200

    first update

--- ExampleProject/FirstHeader.h
+++ ExampleProject/FirstHeader.h
@@ -2,8 +2,8 @@
  * FirstHeader.h
  *
  *  Created on: 28/mar/2018
- *      Author: Raffa hotfix branch 1
- *      Author: Raffaele first hotfix branch2
+ *
+ *      Author: Raffaele
  *
  */
 //hotfix modification
