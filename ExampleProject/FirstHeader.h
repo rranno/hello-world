04090e98a3abcbb1f529c5df2f62813e0f7b4eeb
rranno <rranno@hotmail.it>
2018-03-28 16:39:46 +0200

    modified FirstHeader

--- ExampleProject/FirstHeader.h
+++ ExampleProject/FirstHeader.h
@@ -1,11 +1,11 @@
-/*new comment added
+/*
  * FirstHeader.h
  *
  *  Created on: 28/mar/2018
  *      Author: Giuseppe
  */
 //first change to header file
-//modified
+
 #ifndef FIRSTHEADER_H_
 #define FIRSTHEADER_H_
 
